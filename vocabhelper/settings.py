2c932f9e868f017168690dd5a248323a24b4df54
Rub Ming <rubxiaoming@gmail.com>
2016-04-06 18:11:03 +0800

    Setting DEBUG to True

--- vocabhelper/settings.py
+++ vocabhelper/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#d*c12a^0h$j)nl$!e_3)(_&dwps7dpibq^$60ozdhuboy&a5='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
